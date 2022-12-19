1cd3109e958a6e6cf8e9b917a608bc697c44a84a
HP Computer s <sohailokzz@gmail.com>
2022-12-19 23:23:59 +0500

    complete animation

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -12,11 +14,14 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Parallex Animation',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const BaseWidget(),
+      home: const ParallexAnimation(
+        child: BaseWidget(),
+      ),
     );
   }
 }
@@ -48,6 +53,20 @@ class ParallexAnimation extends StatefulWidget {
 class _ParallexAnimationState extends State<ParallexAnimation> {
   final childKey = GlobalKey();
 
+  Duration animationDuration = const Duration(seconds: 15);
+
+  final initialDelay = Future.delayed(
+    const Duration(seconds: 1),
+  );
+  Timer? animationTimer;
+
+  final parrallexWidthPercent = 120;
+
+  double? totalAdditionalParrallexWidth;
+  double? rightPostion;
+  double maxEndPostion = 0;
+  double maxStartPostion = 0;
+
   late Widget childWithKey;
   double? childBaseWidth;
 
@@ -60,13 +79,24 @@ class _ParallexAnimationState extends State<ParallexAnimation> {
       child: widget.child,
     );
     fetchChildWidth();
+    initTimer();
   }
 
   @override
   Widget build(BuildContext context) {
     return Stack(
       children: [
-        childWithKey,
+        AnimatedPositioned(
+          right: rightPostion,
+          duration: animationDuration,
+          child: SizedBox(
+            width: (childBaseWidth != null &&
+                    totalAdditionalParrallexWidth != null)
+                ? (childBaseWidth! + totalAdditionalParrallexWidth!)
+                : null,
+            child: childWithKey,
+          ),
+        ),
       ],
     );
   }
@@ -77,6 +107,49 @@ class _ParallexAnimationState extends State<ParallexAnimation> {
           childKey.currentContext?.findRenderObject() as RenderBox;
       final childSize = renderBoxRed.size;
       childBaseWidth = childSize.width;
+      initChildPositionWidth();
     });
+  }
+
+  void initChildPositionWidth() {
+    totalAdditionalParrallexWidth =
+        childBaseWidth! * parrallexWidthPercent / 100;
+    rightPostion = -totalAdditionalParrallexWidth! / 2;
+    maxEndPostion = -childBaseWidth! + totalAdditionalParrallexWidth!;
+    maxStartPostion = totalAdditionalParrallexWidth!;
+    setState(() {});
+  }
+
+  void initTimer() async {
+    animationTimer?.cancel();
+    await initialDelay;
+    updateChildPostion();
+    animationTimer = Timer.periodic(
+      animationDuration,
+      (_) {
+        updateChildPostion();
+      },
+    );
+  }
+
+  void updateChildPostion() async {
+    if (rightPostion == 0) {
+      rightPostion = maxEndPostion;
+    } else if (rightPostion == maxEndPostion) {
+      rightPostion = maxStartPostion;
+    } else if (rightPostion == maxStartPostion) {
+      rightPostion = maxEndPostion;
+    } else {
+      rightPostion = maxEndPostion;
+    }
+    setState(() {});
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+
+    ///Closing timer on widget dispose.
+    animationTimer?.cancel();
   }
 }
